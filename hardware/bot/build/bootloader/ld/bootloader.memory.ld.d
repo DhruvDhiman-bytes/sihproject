a4ee205f7927bf61c11fbc205fd84f183a51c8ac
Dhruv Dhiman <dhruvdhiman905@gmail.com>
2026-09-15 14:45:31 +0530

    deleted the build and sdkconfig and re-defined the pin configuration

--- hardware/bot/build/bootloader/ld/bootloader.memory.ld.d
+++
@@ -1,3 +0,0 @@
-/home/dhruv/Desktop/code/sih/bot/build/bootloader/ld/bootloader.memory.ld: \
- /home/dhruv/Desktop/code/firmware/esp-idf/components/bootloader/subproject/main/ld/esp32/bootloader.memory.ld.in \
- /home/dhruv/Desktop/code/sih/bot/build/bootloader/config/sdkconfig.h
